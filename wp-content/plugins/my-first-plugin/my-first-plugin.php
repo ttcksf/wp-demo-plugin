4b2f7aae00189dfde95669f8a84ea6bcb3fc0aa2
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-03-29 06:55:12 +0900

    add:filter-basic

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -2,20 +2,4 @@
 /*
 Plugin Name: My First Plugin
 */
-
-  function add_banner($content){
-    // wp-content/pluginsにてmy-first-pluginを作成
-    // 作成したフォルダ内でmy-first-plugin.phpを作成
-    // 先頭にプラグインネームを記載して、管理画面のプラグインから有効化すると使用できるようになる
-    $html = '<div class="add_banner">バナーです</div>';
-    return $html . $content;
-  }
-  add_filter("the_content", "add_banner");
-
-  function add_banner_css(){
-    echo "<style>";
-    echo ".add_banner{color:red}";
-    echo "</style>";
-  }
-  add_action("wp_head", "add_banner_css");
 ?>
