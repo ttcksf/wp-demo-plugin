ad3a2f54edf28c808370582f1e94a0ed90d13b57
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-03-29 13:23:51 +0900

    add:action-basic

--- wp-content/plugins/my-first-plugin/functions.php
+++ wp-content/plugins/my-first-plugin/functions.php
@@ -1,12 +1,11 @@
 <?php
-function my_first_function($content){
-    // 本文を引数で受け取って末尾に下線を追加する
-    // includes/post-template.phpの256行目
-    // apply_filters("the_content",$content)の部分
-    // apply_filtersはフィルターフックを設置する関数で、第一引数がフック名、第二引数がfunctions.phpで使用するときの引数（コールバックに渡すもの）
-    return $content . "<hr/>";
+  function my_first_function(){
+    // headタグの中にstyleタグを使ったCSSを書く
+    echo "<style>";
+    echo ".single_title h2{color: red;}";
+    echo "</style>";
   }
-  // the_content()関数のthe_contentというイベントで実行
-  add_filter("the_content", "my_first_function");
+  // headタグを出力するwp_head関数で実行する
+  add_filter("wp_head", "my_first_function");
 
 ?>
