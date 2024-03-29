552ae90281c86a6af280e3c83bcc711bdcad5a36
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-03-30 08:50:06 +0900

    add:quick-return

--- wp-content/plugins/my-first-plugin/functions.php
+++ wp-content/plugins/my-first-plugin/functions.php
@@ -1,11 +1,16 @@
 <?php
-  function my_first_function(){
-    // headタグの中にstyleタグを使ったCSSを書く
-    echo "<style>";
-    echo ".single_title h2{color: red;}";
-    echo "</style>";
+  // 以下は同じ意味
+function my_hidden_menu(){
+  return false;
+}
+add_filter("show_admin_bar", "my_hidden_menu");
+add_filter("show_admin_bar", "__return_false");
+
+  // 以下は同じ意味
+  function my_author_rule(){
+    return array();
   }
-  // headタグを出力するwp_head関数で実行する
-  add_action("wp_head", "my_first_function");
+  add_action("author_rewrite_rules", "my_author_rule");
+  add_action("author_rewrite_rules", "__return_empty_array");
 
 ?>
--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -2,33 +2,4 @@
 /*
 Plugin Name: My First Plugin
 */
-
-function create_plugin_page() {  
-  $existing_page = get_page_by_path('slug');
-  
-  if (!$existing_page) {      
-      $page_data = array(
-          'post_type' => 'page',
-          'post_title' => 'プラグイン',
-          'post_name' => 'plugin',
-          'post_status' => 'publish'
-      );
-      wp_insert_post($page_data);
-  }
-}
-register_activation_hook(__FILE__, 'create_plugin_page');
-
-// プラグインが無効化されたときに実行される関数
-function delete_plugin_page() {
-  // スラッグ
-  $page_slug = 'plugin';
-  // スラッグを使ってページを取得します
-  $page = get_page_by_path($page_slug);
-  // ページが存在するかどうかを確認
-  if ($page) {
-      wp_delete_post($page->ID, true);
-  }
-}
-// プラグインが無効化されたときに実行
-register_deactivation_hook(__FILE__, 'delete_plugin_page');
 ?>
