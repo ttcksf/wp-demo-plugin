6e871fde40d6645963736c288ab5305a587e7ad5
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-03-30 02:06:13 +0900

    add:plugin-active

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -3,4 +3,28 @@
 Plugin Name: My First Plugin
 */
 
+function create_member_page() {  
+  // スラグ'plugin'のページが存在するか確認
+  $existing_page = get_page_by_path('slug');
+  
+  if (!$existing_page) {      
+      // 新しいページを作成
+      $page_data = array(
+          // 固定ページ
+          'post_type' => 'page',
+          // タイトル
+          'post_title' => 'プラグイン',
+          // スラッグ
+          'post_name' => 'plugin',
+          // 公開
+          'post_status' => 'publish'
+      );
+      
+      // ページをデータベースに挿入
+      wp_insert_post($page_data);
+  }
+}
+
+// プラグイン有効化時
+register_activation_hook(__FILE__, 'create_member_page');
 ?>
