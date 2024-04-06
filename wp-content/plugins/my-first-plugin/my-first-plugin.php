2f1a68f51157b134f0d60db4ff506f7a5fa4b5a8
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-06 23:26:39 +0900

    eight

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -1,21 +1,31 @@
 <?php
-/*
-Plugin Name: My First Plugin
-*/
+  /*
+  Plugin Name: My First Plugin
+  */
 
-  function add_banner($content){
-    // wp-content/pluginsにてmy-first-pluginを作成
-    // 作成したフォルダ内でmy-first-plugin.phpを作成
-    // 先頭にプラグインネームを記載して、管理画面のプラグインから有効化すると使用できるようになる
-    $html = '<div class="add_banner">バナーです</div>';
-    return $html . $content;
+  function create_plugin_page(){
+    $existing_page = get_page_by_path("plugin");
+    if(!$existing_page){
+      $post_data = [
+        "post_type" => "page",
+        "post_title" => "プラグイン",
+        "post_name" => "plugin",
+        "post_status" => "publish"
+      ];
+      wp_insert_post($post_data);
+    }
   }
-  add_filter("the_content", "add_banner");
+  register_activation_hook(__FILE__, "create_plugin_page");
 
-  function add_banner_css(){
-    echo "<style>";
-    echo ".add_banner{color:red}";
-    echo "</style>";
+  function delete_plugin_page(){
+    $page_slug = "plugin";
+    // pluginというスラッグの固定ページを取得
+    $page = get_page_by_path($page_slug);
+    // 対象の固定ページがあれば削除する
+    if($page){
+      wp_delete_post($page->ID, true);
+    };
   }
-  add_action("wp_head", "add_banner_css");
+  // プラグインを無効化した時に実行する関数
+  register_deactivation_hook(__FILE__, "delete_plugin_page");
 ?>
