79a6a9d7ad76ee4f671a8b231d0894b1ce7e9fca
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-07 09:48:41 +0900

    add:creat-menu

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -3,19 +3,31 @@
 Plugin Name: My First Plugin
 */
 
-  function add_banner($content){
-    // wp-content/pluginsにてmy-first-pluginを作成
-    // 作成したフォルダ内でmy-first-plugin.phpを作成
-    // 先頭にプラグインネームを記載して、管理画面のプラグインから有効化すると使用できるようになる
-    $html = '<div class="add_banner">バナーです</div>';
-    return $html . $content;
+  function my_admin_menu(){
+    add_menu_page(
+      // $page_title　：　ページタイトル（title）,
+      '新メニュー設定画面',
+      // $menu_title　：　メニュータイトル,
+      '新メニュー',
+      // $capability　：　メニュー表示するユーザーの権限,
+      'manage_options',
+      // $menu_slug,　：　メニューのスラッグ,
+      'new_menu',
+      // $function,　：　メニュー表示時に使われる関数,
+      'render_new_menu',
+      // $icon_url,　：　メニューのテキスト左のアイコン,
+      'dashicons-admin-generic',
+      // $position 　：　メニューを表示する位置;
+      // https://developer.wordpress.org/reference/functions/add_menu_page/
+      99
+    );
   }
-  add_filter("the_content", "add_banner");
-
-  function add_banner_css(){
-    echo "<style>";
-    echo ".add_banner{color:red}";
-    echo "</style>";
+  function render_new_menu(){
+    ?>
+    <div class="wrap">
+      <h2>新メニュー</h2>
+    </div>
+    <?php
   }
-  add_action("wp_head", "add_banner_css");
+  add_action("admin_menu", "my_admin_menu");
 ?>
