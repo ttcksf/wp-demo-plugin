47a3d070f9e0b254c332a1b197c3d75e63d3f122
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-07 10:55:58 +0900

    add:admin-menu-form-continue

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -26,28 +26,48 @@ Plugin Name: My First Plugin
     do_settings_sections('admin-menu-form'); ?>
     <!-- WordPressのスタイルを使うにはclass名を揃える必要がある -->
     <div class="metabox-holder">
-      <div class="postbox">
-        <h3 class='hndle'><span>テキスト</span></h3>
+      <div class="postbox ">
+        <h3 class='hndle'><span>セレクトボックス</span></h3>
         <div class="inside">
           <div class="main">
-            <p class="setting_description">テキストを入力してください。</p>
-            <h4>テキスト</h4>
-            <p><input type="text" id="text" name="text" value="<?php echo get_option('text'); ?>"></p>
+            <p class="setting_description">セレクトボックスを選択してください。</p>
+            <h4>セレクトボックス</h4>
+            <select name="select" id="select">
+              <option value="0" <?php selected(0, get_option('select')); ?> >選択してください</option>
+              <option value="1" <?php selected(1, get_option('select')); ?> >セレクトボックス1</option>
+              <option value="2" <?php selected(2, get_option('select')); ?> >セレクトボックス2</option>
+              <option value="3" <?php selected(3, get_option('select')); ?> >セレクトボックス3</option>
+            </select>
           </div>
         </div>
       </div>
-      <div class="postbox">
-        <h3 class='hndle'><span>テキストボックス</span></h3>
+      <div class="postbox ">
+        <h3 class='hndle'><span>ラジオボタン</span></h3>
         <div class="inside">
           <div class="main">
-            <p class="setting_description">テキストボックスを入力してください。</p>
-            <h4>テキストボックス</h4>
-            <textarea id="textbox" class="regular-text" name="textbox" rows="10" cols="60"><?php echo get_option('textbox'); ?></textarea>
+            <p class="setting_description">ラジオボタンを選択してください。</p>
+            <h4>ラジオボタン</h4>
+            <ul>
+              <li><label><input name="radio" type="radio" value="0" <?php checked(0, get_option('radio')); ?> />ラジオボタン0</label></li>
+              <li><label><input name="radio" type="radio" value="1" <?php checked(1, get_option('radio')); ?> />ラジオボタン1</label></li>
+              <li><label><input name="radio" type="radio" value="2" <?php checked(2, get_option('radio')); ?> />ラジオボタン2</label></li>
+              <li><label><input name="radio" type="radio" value="3" <?php checked(3, get_option('radio')); ?> />ラジオボタン3</label></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="postbox ">
+        <h3 class='hndle'><span>チェックボックス</span></h3>
+        <div class="inside">
+          <div class="main">
+            <p class="setting_description">チェックボックスをチェックしてください。</p>
+            <label><input name="checkbox" id="checkbox1" type="checkbox" value="1" <?php checked(1, get_option('checkbox')); ?> />チェックボックス1</label>
+            <label><input name="checkbox" id="checkbox1" type="checkbox" value="1" <?php checked(1, get_option('checkbox')); ?> />チェックボックス2</label>
+            <label><input name="checkbox" id="checkbox1" type="checkbox" value="1" <?php checked(1, get_option('checkbox')); ?> />チェックボックス3</label>
           </div>
         </div>
       </div>
     </div>
-    <!-- ボタンを表示する関数 -->
     <?php submit_button(); ?>
   </form>
 </div>
