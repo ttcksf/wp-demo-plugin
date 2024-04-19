5667fe08c031b4a812c25e9d909c11ef9fb08557
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-20 04:45:33 +0900

    add:media-tips

--- wp-content/plugins/my-first-plugin/functions.php
+++ wp-content/plugins/my-first-plugin/functions.php
@@ -1,7 +1,15 @@
 <?php
 
-  // 固定ページに抜粋文を追加する
-  add_post_type_support("page", "excerpt");
-  // 投稿ページにページ属性を追加する
-  add_post_type_support("post", "page-attributes");
+  // アップロードできるファイルの種類を制限
+  function my_mime_type($mime_types){
+    $mime_types = [
+      // キーは拡張子を正規表現で値はMIMEタイプ
+      // https://developer.wordpress.org/reference/hooks/mime_types/
+      "png" => "image/png",
+      "jpg|jpeg|jpe" => "image/jpeg",
+      "pdf" => "application/pdf",
+    ];
+    return $mime_types;
+  }
+  add_filter("upload_mimes", "my_mime_type",1,1);
 ?>
