7de6c8bdb8ea6a491afe4b707fe26f89a0672319
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-20 00:57:58 +0900

    add:enqueue-style-script

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -1,3 +1,18 @@
 <?php
 /* Plugin Name: My First Plugin */
+
+// プラグインが有効化された時にだけ実行するCSS,JSファイル
+// function add_scripts(){
+//   wp_enqueue_script("my-script", plugins_url("js/plugin.js", __FILE__));
+// }
+// add_action("wp_enqueue_scripts", "add_scripts");
+
+// 管理画面のみで実行したい時
+function add_scripts($hook){
+  if("edit.php" != $hook){
+    return;
+  }
+  wp_enqueue_script("my-script", plugins_url("js/plugin.js", __FILE__));
+}
+add_action("admin_enqueue_scripts", "add_scripts");
 ?>
