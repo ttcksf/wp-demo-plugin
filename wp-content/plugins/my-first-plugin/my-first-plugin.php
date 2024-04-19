60f89b6c64b6dfe22edf3ce3515de3f293892278
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-19 22:17:46 +0900

    ten

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -1,8 +1,5 @@
 <?php
-/*
-Plugin Name: My First Plugin
-*/
-
+/* Plugin Name: My First Plugin */
   function my_admin_menu(){
     add_menu_page(
       "新メニュー設定画面",
@@ -13,103 +10,47 @@ Plugin Name: My First Plugin
       "dashicons-admin-generic",
       99
     );
+    add_action("admin_init", "register_custom_form");
   }
   add_action("admin_menu", "my_admin_menu");
 
   function render_new_menu(){
     ?>
-      <h1>新メニュー設定画面</h1>
-      <form action="" method="post" enctype="multipart/form-data" encoding="multipart/form-data">
-        <?php settings_fields("admin-menu-form"); ?>
-        <?php do_settings_sections("admin-menu-form");?>
-
+      <h1>新メニュー</h1>
+      <form method="post" action="">
+        <?php  
+          settings_fields("admin-menu-form");
+          do_settings_sections("admin-menu-form");
+        ?>
         <div class="metabox-holder">
           <div class="postbox">
-            <h3 class="hndle">
-              <span>セレクトボックス</span>
-            </h3>
+            <h3 class="hndle"><span>テキスト</span></h3>
             <div class="inside">
               <div class="main">
-                <p class="setting_description">選択形式の質問項目です</p>
-                <h4>セレクトボックス</h4>
-                <select name="select" id="select">
-                  <option value="0" <?php selected(0, get_option("select"));?>>選択してください</option>
-                  <option value="1" <?php selected(1, get_option("select"));?>>アイテムA</option>
-                  <option value="2" <?php selected(2, get_option("select"));?>>アイテムB</option>
-                  <option value="3" <?php selected(3, get_option("select"));?>>アイテムC</option>
-                </select>
+                <p class="setting_description">テキストを入力してください</p>
+                <p><input type="text" id="text" name="text" value="<?php echo esc_attr(get_option("text")) ?>" /></p>
               </div>
             </div>
           </div>
-          <div class="postbox">
-            <h3 class="hndle">
-              <span>ラジオボタン</span>
-            </h3>
-            <div class="inside">
-              <div class="main">
-                <p class="setting_description">選択形式の質問項目です</p>
-                <h4>ラジオボタン</h4>
-                <ul>
-                  <li>
-                    <label>
-                      <input type="radio" name="radio" value="0" <?php checked(0, get_option("radio"));?>/>
-                      ラジオボタン0
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" name="radio" value="1" <?php checked(1, get_option("radio"));?>/>
-                      ラジオボタン1
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" name="radio" value="2" <?php checked(2, get_option("radio"));?>/>
-                      ラジオボタン2
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" name="radio" value="3" <?php checked(3, get_option("radio"));?>/>
-                      ラジオボタン3
-                    </label>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="postbox">
-            <h3 class="hndle">
-              <span>チェックボックス</span>
-            </h3>
-            <div class="inside">
-              <div class="main">
-                <p class="setting_description">選択形式の質問項目です</p>
-                <h4>チェックボックス</h4>
-                <label for="">
-                  <input type="checkbox" id="checkbox1" name="checkbox" value="0" <?php checked(0, get_option("checkbox"));?>>
-                  選択肢0
-                </label>
-                <label for="">
-                  <input type="checkbox" id="checkbox1" name="checkbox" value="1" <?php checked(1, get_option("checkbox"));?>>
-                  選択肢1
-                </label>
-                <label for="">
-                  <input type="checkbox" id="checkbox1" name="checkbox" value="2" <?php checked(2, get_option("checkbox"));?>>
-                  選択肢2
-                </label>
-              </div>
-            </div>
-          </div>
-          <?php submit_button();?>
         </div>
+        <?php submit_button();?>
       </form>
     <?php
   }
 
   function register_custom_form(){
     register_setting("admin-menu-form", "text");
-    register_setting("admin-menu-form", "textbox");
   }
-  add_action("admin_init", "register_custom_form");
+
+  function admin_form_submit(){
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+      if(isset($_POST["text"]) && $_POST["text"]){
+        update_option("text", $_POST["text"]);
+      }else{
+        update_option("text", "");
+      }
+      wp_safe_redirect(menu_page_url("new_menu", false));
+    }
+  }
+  add_action("admin_init","admin_form_submit");
 ?>
